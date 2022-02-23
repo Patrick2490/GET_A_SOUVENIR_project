d970e20d34d9568fa9a42b8a338d372c25ad5553
Patrick2490 <patrick2490as@gmail.com>
2022-02-23 13:37:31 +0300

    deployed on heroku

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ app_name = 'api'
 urlpatterns = [
     path('souvenirs/', views.ListSouvenirAPIView.as_view(), name='souvenirs'),
     path('souvenirs/<int:pk>/', views.DetailSouvenirAPIView.as_view()),
-    path('user_infos/', views.ListUserInfoApiView.as_view()),
+    path('user_infos/', views.ListUserInfoApiView.as_view(), name='user_infos'),
     path('user_infos/<int:pk>/', views.DetailUserInfoApiView.as_view()),
 ]
